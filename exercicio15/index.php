9491cbc77d80ddae5573d7300e9f8d1c35f91bd1
Michele Farias <mi.farias182@gmail.com>
2022-05-28 07:14:04 -0400

    decimo quinto exercicio

---
+++ exercicio15/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 15</title>
+</head>
+<body>
+    <?php
+        echo"<h2>Os múltiplos são:</h2>";      
+        for($i=0; $i<=100; $i++){
+            if($i%2 == 0){
+                echo " {$i} ";
+            }
+              
+                          
+                     
+        }
